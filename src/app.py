0e422663a050c375aea0fee67280a973d7a548be
rapharibeirawj <86366204+rapharibeirawj@users.noreply.github.com>
2025-06-25 19:59:09 +0000

    Fix comment for clarity in signup_for_activity function

--- src/app.py
+++ src/app.py
@@ -101,7 +101,7 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-    # Check if student already signed up
+    # Check if student already signed up 1
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already signed up for this activity")
 
